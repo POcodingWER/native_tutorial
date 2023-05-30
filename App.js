c60e43025458e525ac8e7a594958fd631a92cb22
lim.cs <cs.lim@fingerverse.co.kr>
2023-05-30 14:07:05 +0900

    flex 사용법

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Text } from "react-native";
-
+import { View } from "react-native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Helloooooooooo</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-/* 브라우저가아니라 h1,span,b 등등 태그사용할수없음*/
-//StyleSheet.create사용해야 자동맞춤이 발생함
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "black",
-  },
-});
