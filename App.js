45025cd10dc14d47fbd8c1711bb1ccb1e976558b
lim.cs <cs.lim@fingerverse.co.kr>
2023-05-30 12:12:09 +0900

    스타일 auto complete 태그사용불가

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Image, Text } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Helloooooooooo</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+/* 브라우저가아니라 h1,span,b 등등 태그사용할수없음*/
+//StyleSheet.create사용해야 자동맞춤이 발생함
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
   },
 });
